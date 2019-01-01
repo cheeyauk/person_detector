13677568fbe20b2a729d5c539ef65c6697051a0b
cheeyau <cykee3@gmail.com>
2019-01-01 18:11:49 +0800

    update sample files

--- sample.py
+++ sample.py
@@ -1,9 +1,15 @@
+###############################################
+# Author : Chee Yau
+# Last Modified : 1 Janurayr 2019
+##############################################
+
+
 from human_detector import DetectorAPI
 import cv2
 
-detector = DetectorAPI("mobilenet_v2lite.pb") # pass in your path here
+detector = DetectorAPI("mobilenet_v2lite.pb") # pass in your model path here
 
-img = cv2.imread('sample_img2.jpg') # img provided. please use for sample purpose only :)
+img = cv2.imread('sample_img.jpg') # img provided. please use for sample purpose only :)
 
 # Switch to processFrame to get the boxes and scores programmatically, and filter the classes = 1 (person class)
 processedImg = detector.processImage(img)
